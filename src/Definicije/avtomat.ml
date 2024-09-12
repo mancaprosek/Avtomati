3b989e942ba5a2aac4fdb8b8b73e32cb72e8e686
manca <63961724+mancaprosek@users.noreply.github.com>
2024-09-12 12:56:34 +0200

    Popravek prehodov

--- src/Definicije/avtomat.ml
+++ src/Definicije/avtomat.ml
@@ -50,15 +50,19 @@ let enke_1mod3 =
   and q2 = Stanje.iz_niza "q2"
   and q3 = Stanje.iz_niza "q3"
   and q4 = Stanje.iz_niza "q4" in
-  prazen_avtomat q0 |> dodaj_sprejemno_stanje q0 |> dodaj_sprejemno_stanje q1
-  |> dodaj_sprejemno_stanje q2 |> dodaj_sprejemno_stanje q3 |> dodaj_nesprejemno_stanje q4
+  prazen_avtomat q0
+  |> dodaj_sprejemno_stanje q0
+  |> dodaj_sprejemno_stanje q1
+  |> dodaj_sprejemno_stanje q2
+  |> dodaj_sprejemno_stanje q3
+  |> dodaj_nesprejemno_stanje q4
   |> dodaj_prehod q0 '1' q1 |> dodaj_prehod q1 '1' q2 |> dodaj_prehod q2 '1' q3 |> dodaj_prehod q3 '1' q4 |> dodaj_prehod q4 '1' q4
   |> dodaj_prehod q0 '2' q1 |> dodaj_prehod q1 '2' q2 |> dodaj_prehod q2 '2' q3 |> dodaj_prehod q3 '2' q4 |> dodaj_prehod q4 '2' q4
   |> dodaj_prehod q0 '3' q1 |> dodaj_prehod q1 '3' q2 |> dodaj_prehod q2 '3' q3 |> dodaj_prehod q3 '3' q4 |> dodaj_prehod q4 '3' q4
   |> dodaj_prehod q0 '4' q1 |> dodaj_prehod q1 '4' q2 |> dodaj_prehod q2 '4' q3 |> dodaj_prehod q3 '4' q4 |> dodaj_prehod q4 '4' q4
   |> dodaj_prehod q0 '5' q0 |> dodaj_prehod q1 '5' q1 |> dodaj_prehod q2 '5' q2 |> dodaj_prehod q3 '5' q3 |> dodaj_prehod q4 '5' q4
-  |> dodaj_prehod q4 '0' q0
-  
+  |> dodaj_prehod q0 '0' q0 |> dodaj_prehod q1 '0' q0 |> dodaj_prehod q2 '0' q0 |> dodaj_prehod q3 '0' q0 |> dodaj_prehod q4 '0' q0
+
 let preberi_niz avtomat q niz =
   let aux acc znak =
     match acc with
